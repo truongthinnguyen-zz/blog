866aa6ac34726274b30cdc8c8420fcb1224e0c34
Nguyen Truong Thin <truongthinnguyen@gmail.com>
2015-08-22 20:50:16 +0700

    tienganh123

---
+++ public/demos/tienganh123/inject-tienganhonline.js
@@ -0,0 +1,6 @@
+$('script[src^="http://www.tienganh123.com/static/js/libs_audio.js"]').remove();
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '';
+
+document.getElementsByTagName('head')[0].appendChild(script);
