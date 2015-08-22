de40b7f4a6396631d90670d9ff4bd738d10ce502
Nguyen Truong Thin <truongthinnguyen@gmail.com>
2015-08-22 20:56:20 +0700

    change url

--- public/demos/tienganh123/inject-tienganhonline.js
+++ public/demos/tienganh123/inject-tienganhonline.js
@@ -1,6 +1,6 @@
 $('script[src^="http://www.tienganh123.com/static/js/libs_audio.js"]').remove();
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'demo';
+script.src = 'https://shrouded-inlet-5864.herokuapp.com/demos/tienganh123/libs_audio.js';
 
 document.getElementsByTagName('head')[0].appendChild(script);
